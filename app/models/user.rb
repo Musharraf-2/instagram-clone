5ec7c4e0f2ed78568bee3963f771b6946754088c
Musharraf-2 <musharraf.tahir@devsinc.com>
2022-11-02 12:40:30 +0500

    Post module completed * navbar.css: added styling for navabr * ApplicationController: added before_action callback for authenticate_user * post.css: added styling for post views * PostController: added index action method * PostController: added show action method * PostController: added new action method * PostController: added edit action method * PostController: added create action method * PostController: added edit action method * PostController: added destroy action method * routes.rb: added route for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :registerable
 
   has_one_attached :image
+  has_many :posts, dependent: :destroy
 
   validates :image, attached: true, content_type: ['image/png', 'image/jpeg']
   validates :username, presence: true, uniqueness: true
